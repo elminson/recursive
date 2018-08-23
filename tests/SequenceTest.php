5bc8e0f4f1005ad96ddb52a9e1393f808ad3f1ca
Elminson De Oleo Baez <edeoleo@gmail.com>
2018-08-23 01:12:20 -0400

    Adding Test cases

--- tests/SequenceTest.php
+++ tests/SequenceTest.php
@@ -1,9 +1,68 @@
 <?php
 /**
- * Created by PhpStorm.
  * User: Elminson De Oleo Baez
- * Filename: ${FILENAME}
- * Desc: ...
  * Date: 8/22/2018
  * Time: 10:57 PM
  */
+
+namespace Recursive;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use PHPUnit\Exception;
+use PHPUnit\Framework\TestCase;
+
+class SequenceTest extends TestCase
+{
+    public function testTrueSequence()
+    {
+        $sequence = new Sequence();
+        $array = [1, 2, 3, 4, 5, 6, 7, 8];
+        $this->assertTrue($sequence->check_sequence($array, 3, count($array) - 1));
+    }
+
+    public function testTrueDebbugSequence()
+    {
+        $sequence = new Sequence(true);
+        $array = [1, 2, 3, 4, 5, 6, 7, 8];
+        $this->assertTrue($sequence->check_sequence($array, 3, count($array) - 1));
+    }
+
+    public function testTrueTwoSequence()
+    {
+        $sequence = new Sequence();
+        $array = [1, 2, 3, 4, 5, 6, 7, 8];
+        $this->assertTrue($sequence->check_sequence($array, 1, count($array) - 1));
+    }
+
+    public function testFalseSequence()
+    {
+        $sequence = new Sequence();
+        $array = [1, 3, 5, 7, 8];
+        $this->assertFalse($sequence->check_sequence($array, 3, count($array) - 1));
+    }
+
+    public function testExceptionSequence()
+    {
+        $sequence = new Sequence();
+        $array = [];
+        $this->expectException(Exception::class, "Array can't be empty!");
+        $sequence->check_sequence($array, 3, count($array) - 1);
+    }
+
+    public function testExceptionNullSequence()
+    {
+        $sequence = new Sequence();
+        $array = [""];
+        $this->expectException(Exception::class, "Array can't be empty!");
+        $sequence->check_sequence($array, 3, count($array) - 1);
+    }
+
+    public function testExceptionNotNumericSequence()
+    {
+        $sequence = new Sequence();
+        $array = ["a", 2, 3, "a"];
+        $this->expectException(Exception::class, "All elements must be integers!");
+        $sequence->check_sequence($array, 3, count($array) - 1);
+    }
+}
