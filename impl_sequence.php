1e1dc3403c65815e1feb9cb21041091bf2cc7042
Elminson De Oleo Baez <edeoleo@gmail.com>
2018-08-23 01:13:18 -0400

    Adding Test file

--- impl_sequence.php
+++ impl_sequence.php
@@ -1,9 +1,14 @@
 <?php
 /**
- * Created by PhpStorm.
  * User: Elminson De Oleo Baez
- * Filename: ${FILENAME}
- * Desc: ...
  * Date: 8/23/2018
  * Time: 12:18 AM
  */
+
+namespace Recursive;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$new = new Sequence();
+$array=[1,2,3,4];
+echo $new->check_sequence($array,3,count($array));
