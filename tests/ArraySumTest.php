cd85e39dce36a877dac787840ff71eb8f8558b06
Elminson De Oleo Baez <edeoleo@gmail.com>
2018-08-23 12:46:02 -0400

    Adding test cases for arraysum class

---
+++ tests/ArraySumTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * User: Elminson De Oleo Baez
+ * Date: 8/22/2018
+ * Time: 10:57 PM
+ */
+
+namespace Recursive;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use PHPUnit\Exception;
+use PHPUnit\Framework\TestCase;
+
+class ArraySumTest extends TestCase
+{
+    public function testArraySum()
+    {
+        $array_sum = new ArraySum();
+        $array = [1, 2, 3];
+        $this->assertEquals(6, $array_sum->sum($array, count($array) - 1));
+    }
+}
